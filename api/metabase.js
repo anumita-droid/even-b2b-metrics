e5faa2344a59738a393350d9c50ebf39b8f39a55
anumita-droid <anumita@even.in>
2026-09-24 19:56:08 +0530

    Use published Master List for dashboard

--- api/metabase.js
+++ api/metabase.js
@@ -1,5 +1,5 @@
 const PUBLISHED_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQf3juS1A7q2aaxozajUWIrZgpsWUpHMl0XTS0H0nGa3RgF4oRIfDv2vBx8bqL9ltEFSFq1Q_ZJvAdT/pub';
-const MASTER_ID = '1Ovf4qHm398T3tiGpSFmE1jf354nTPB5rtkgYeFt1w00';
+const MASTER_PUBLISHED_URL = String.fromCharCode(104,116,116,112,115,58,47,47,100,111,99,115,46,103,111,111,103,108,101,46,99,111,109,47,115,112,114,101,97,100,115,104,101,101,116,115,47,100,47,101,47,50,80,65,67,88,45,49,118,81,102,55,67,84,122,118,69,49,102,79,52,48,87,104,73,48,69,79,51,120,77,111,65,73,70,75,73,88,56,85,48,74,97,119,74,52,114,49,48,101,69,67,116,78,49,100,101,116,81,66,108,103,117,73,84,78,76,119,89,68,102,88,104,78,98,112,118,72,75,78,65,122,119,100,55,66,102,47,112,117,98);
 
 const GIDS = {
   ri_cl_combined: 1860220766,
@@ -24,11 +24,11 @@ export default async function handler(req, res) {
 
   const { tab, master } = req.query;
 
-  // Master List (still uses the existing source for now)
+  // Published Master List
   if (master === '1') {
     try {
       const r = await fetch(
-        `https://docs.google.com/spreadsheets/d/${MASTER_ID}/export?format=csv&gid=0`
+        `${MASTER_PUBLISHED_URL}?output=csv`
       );
       const text = await r.text();
 
